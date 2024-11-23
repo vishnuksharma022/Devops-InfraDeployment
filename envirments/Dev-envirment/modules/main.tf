b395a75762fcb1051ea01ca2c76842f037f2a169
vishnu <vishnu@ymail.com>
2024-11-23 18:04:44 +0530

    update-on module

--- envirments/Dev-envirment/modules/main.tf
+++ envirments/Dev-envirment/modules/main.tf
@@ -2,3 +2,16 @@ module "RG" {
   source    = "../RG"
   Devops-RG = var.Devops-RG
 }
+
+module "vnet" {
+  depends_on = [ var.Devops-RG ]
+  source = "../Vnet-subnet"
+  azure-vnet= var.azure-vnet
+  azure-subnet= var.azure-subnet
+}
+
+module "nic" {
+  depends_on = [ var.azure-subnet ]
+  source = "../Nic-VM"
+  azure-nic= var.azure-nic 
+}
