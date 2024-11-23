aacee7307766f23d81c9cde2b28831d7cff0054f
vishnu <vishnu@ymail.com>
2024-11-23 17:11:49 +0530

    code update

--- envirments/Dev-envirment/RG/main.tf
+++ envirments/Dev-envirment/RG/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "RG" {
-    for_each = var.Devops-RG
-    name = each.value.azurerm_resource_group
-    location = each.value.location
-  
+  for_each = var.Devops-RG
+  name     = each.value.azurerm_resource_group
+  location = each.value.location
+
 }
